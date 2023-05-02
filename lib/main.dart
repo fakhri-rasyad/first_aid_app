1be306020d65b959f4e6d22d34507a5b424388b0
Fakhri Rasyad <kaizerd23@gmail.com>
2023-05-02 11:03:07 +0800

    02/05/2023 11:02
    Perbaikan fitur filter dan penambahan floating action button

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,7 @@ class _MainPageState extends State<MainPage> {
     final data = await json.decode(response);
     setState(() {
       _pertolonganPertama = data;
+      _filtered = _pertolonganPertama;
     });
   }
 
@@ -53,77 +54,82 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Aplikasi P3K'),
-        ),
-        body: Container(
-          padding: EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                margin: EdgeInsets.only(bottom: 24.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SizedBox(
-                      width: 240,
-                      child: TextField(
-                        onChanged: (value) => setState(() {
-                          userInput = value;
-                          _filtered = _pertolonganPertama
-                              .where((element) => (element["judul"]
-                                  .toString()
-                                  .toLowerCase()
-                                  .contains(userInput.toLowerCase())))
-                              .toList();
-                        }),
-                        controller: _searchController,
-                        decoration: InputDecoration(
-                          hintText: 'Cari Pertolongan Pertama',
-                        ),
+      appBar: AppBar(
+        title: Text('Aplikasi P3K'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 24.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 240,
+                    child: TextField(
+                      onChanged: (value) => setState(() {
+                        userInput = value;
+                        _filtered = _pertolonganPertama
+                            .where((element) => (element["judul"]
+                                .toString()
+                                .toLowerCase()
+                                .contains(userInput.toLowerCase())))
+                            .toList();
+                      }),
+                      controller: _searchController,
+                      decoration: InputDecoration(
+                        hintText: 'Cari Pertolongan Pertama',
                       ),
                     ),
-                    IconButton(
-                        onPressed: () {
-                          setState(() {
-                            _isInput = !_isInput;
-                          });
-                        },
-                        icon: Icon(Icons.search)),
-                  ],
-                ),
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _isInput = !_isInput;
+                        });
+                      },
+                      icon: Icon(Icons.search)),
+                ],
               ),
-              Expanded(
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  shrinkWrap: true,
-                  children: List.generate(_filtered.length, (index) {
-                    return Card(
-                      child: Column(
-                        textDirection: TextDirection.ltr,
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Image.asset(
-                            _filtered[index]["gambar"],
-                            width: 180,
-                            height: 90,
+            ),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                children: List.generate(_filtered.length, (index) {
+                  return Card(
+                    child: Column(
+                      textDirection: TextDirection.ltr,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image.asset(
+                          _filtered[index]["gambar"],
+                          width: 180,
+                          height: 90,
+                        ),
+                        Text(
+                          _filtered[index]["judul"],
+                          style: TextStyle(
+                            fontSize: 16.0,
                           ),
-                          Text(
-                            _filtered[index]["judul"],
-                            style: TextStyle(
-                              fontSize: 16.0,
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  }),
-                ),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
               ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.room_preferences_outlined),
+      ),
+    );
   }
 }
